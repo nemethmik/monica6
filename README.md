dbd7ff0de00c26b83058ce5cc9e87ae4070820ce
Nemeth Miklos <nemethmiklos1960@gmail.com>
2021-09-22 14:32:36 +0200

    feat(main): Add Vaadin app layout sample

--- README.md
+++ README.md
@@ -5,6 +5,10 @@ Main Branch is initialized with *Vite* as `npm init vite`, *tsconfig.json* modif
 - Commit the files to the local repo
 - and then publish to GitHub with clicking the cloud icon on the bottom left corner next to the master branch name.
 
+## Vaadin App Layout Basic Sample 
+This is the remake of [Vaadin's App Layout Samples](https://vaadin.com/docs/latest/ds/components/app-layout) in branch *monica02applayout*
+- I had to downgrade TypeScript from 4.4 to 4.3 because of some weird typing issues in the Vaadin library: `npm i -D typescript@4.3` 
+- `npm i @vaadin/vaadin-app-layout @vaadin/vaadin-icons @vaadin/vaadin-tabs` These three packages were enough.
 
-## Vaadin Toto Tutorial 
+## Vaadin Todo Tutorial 
 In branch *monica01todo*
--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,89 @@
-import "./style.css"
-import {html,render,LitElement, TemplateResult} from "lit"
+//import "./style.css"
+import {html,css, CSSResult, render,LitElement, TemplateResult} from "lit"
 import {customElement} from "lit/decorators.js"
-
+import "@vaadin/vaadin-app-layout/vaadin-app-layout"
+import "@vaadin/vaadin-app-layout/vaadin-drawer-toggle"
+import "@vaadin/vaadin-icon/vaadin-icon"
+import "@vaadin/vaadin-icons/vaadin-iconset"
+import "@vaadin/vaadin-tabs/vaadin-tab"
+import "@vaadin/vaadin-tabs/vaadin-tabs"
+//import { applyTheme } from 'Frontend/generated/theme';
 const app = document.querySelector<HTMLDivElement>("#app")!
 render(html`<my-app></my-app>`,app)
 
 @customElement("my-app")
-class App extends LitElement {
-  override render():TemplateResult {
+export class App extends LitElement {
+  // protected createRenderRoot():Element | ShadowRoot {
+  //   const root = super.createRenderRoot()
+  //   // Apply custom theme (only supported if your app uses one)
+  //   //applyTheme(root);
+  //   return root
+  // }
+  static get styles():CSSResult {
+    return css`
+      h1 {
+        font-size: var(--lumo-font-size-l);
+        margin: 0;
+      }
+      vaadin-icon {
+        box-sizing: border-box;
+        margin-inline-end: var(--lumo-space-m);
+        margin-inline-start: var(--lumo-space-xs);
+        padding: var(--lumo-space-xs);
+      }
+    `
+  }
+  render():TemplateResult {
     return html`
-      <h1>Hello Lit!</h1>
-      <a href="https://lit.dev/" target="_blank">Documentation</a>
-    `   
+      <vaadin-app-layout>
+        <vaadin-drawer-toggle slot="navbar"></vaadin-drawer-toggle>
+        <h1 slot="navbar">My Vaadin App Layout Sample</h1>
+        <vaadin-tabs slot="drawer" orientation="vertical">
+          <vaadin-tab>
+            <a tabindex="-1">
+              <vaadin-icon icon="vaadin:dashboard"></vaadin-icon>
+              <span>Dashboard</span>
+            </a>
+          </vaadin-tab>
+          <vaadin-tab>
+            <a tabindex="-1">
+              <vaadin-icon icon="vaadin:cart"></vaadin-icon>
+              <span>Orders</span>
+            </a>
+          </vaadin-tab>
+          <vaadin-tab>
+            <a tabindex="-1">
+              <vaadin-icon icon="vaadin:user-heart"></vaadin-icon>
+              <span>Customers</span>
+            </a>
+          </vaadin-tab>
+          <vaadin-tab>
+            <a tabindex="-1">
+              <vaadin-icon icon="vaadin:package"></vaadin-icon>
+              <span>Products</span>
+            </a>
+          </vaadin-tab>
+          <vaadin-tab>
+            <a tabindex="-1">
+              <vaadin-icon icon="vaadin:records"></vaadin-icon>
+              <span>Documents</span>
+            </a>
+          </vaadin-tab>
+          <vaadin-tab>
+            <a tabindex="-1">
+              <vaadin-icon icon="vaadin:list"></vaadin-icon>
+              <span>Tasks</span>
+            </a>
+          </vaadin-tab>
+          <vaadin-tab>
+            <a tabindex="-1">
+              <vaadin-icon icon="vaadin:chart"></vaadin-icon>
+              <span>Analytics</span>
+            </a>
+          </vaadin-tab>
+        </vaadin-tabs>
+      </vaadin-app-layout>
+    `
   }
 }
 declare global { interface HTMLElementTagNameMap { "my-app": App}}
